9682298260e50e128f81f05ef771e1588381f50f
majdi <you@example.com>
2022-10-26 22:36:02 +0100

    first commit

---
+++ mario.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        
+        for (int j = 0; j < i + 1; j++)
+        {
+        printf("#");
+        }
+        printf("\n");
+    }
+    // }
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     printf("#");
+    //     printf("\n");
+    // }
+}
+
